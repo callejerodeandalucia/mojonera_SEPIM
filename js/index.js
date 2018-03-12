c02436c4afdf8e913462e0a0622f120f6f344846
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2018-03-12 09:31:58 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ function geolocalizar(){
 			  cargarCategoria();
 		  };
 		  var errorFunction = function(){
-			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Carrión");
+			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: La Mojonera");
 			  loading(false);
 		  };
 		  loading(true);	
